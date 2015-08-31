bbd1272788df483cdee7281bfb2551a50a07df54
qzw <www.qzw87.com>
2015-08-31 14:23:25 +0800

    初始化项目

---
+++ 2015083101/2015083101/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2015083101
+//
+//  Created by qzw on 15/8/31.
+//  Copyright (c) 2015年 qzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2015083101/2015083101Tests/_015083101Tests.m
@@ -0,0 +1,40 @@
+//
+//  _015083101Tests.m
+//  2015083101Tests
+//
+//  Created by qzw on 15/8/31.
+//  Copyright (c) 2015年 qzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _015083101Tests : XCTestCase
+
+@end
+
+@implementation _015083101Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
